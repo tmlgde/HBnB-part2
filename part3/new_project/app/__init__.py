dd0d6bfd3d5446f904443fcd1008019a81b864ca
ilmi-veliu <iveliu011@gmail.com>
2025-07-02 12:32:21 +0200

    task before 0

--- part3/new_project/app/__init__.py
+++ part3/new_project/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
 from app.config import DevelopmentConfig
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
@@ -8,11 +10,14 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
     bcrypt.init_app(app)
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
---
+++ part3/new_project/repositories/sqlalchemy_repository.py
@@ -0,0 +1,31 @@
+from app import db
+
+class SQLAlchemyRepository:
+    def __init__(self, model):
+        self.model = model
+
+    def add(self, obj):
+        db.session.add(obj)
+        db.session.commit()
+
+    def get(self, obj_id):
+        return self.model.query.get(obj_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
+        return obj
+
+    def delete(self, obj_id):
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
+            return True
+        return False
